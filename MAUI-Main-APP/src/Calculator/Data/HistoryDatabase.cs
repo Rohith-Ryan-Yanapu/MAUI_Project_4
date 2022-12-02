448a274cc9755418934f1fd09336e3b342abb80d
prudhvireddymali <prudhvireddymali2021@gmail.com>
2022-12-02 03:59:32 -0600

    Added Insert Funtion

---
+++ MAUI-Main-APP/src/Calculator/Data/HistoryDatabase.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using Calculator.Models;
+namespace Calculator.Data;
+
+public class HistoryDatabase
+{
+    SQLiteAsyncConnection Database;
+    async Task Init()
+    {
+        if (Database is not null)
+            return;
+
+        Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        var result = await Database.CreateTableAsync<HistoryItem>();
+    }
+
+    public async Task<int> SaveItemAsync(String exp)
+    {
+        await Init();
+        return await Database.InsertAsync(new HistoryItem { Expression = exp });
+    }
+    
+}
